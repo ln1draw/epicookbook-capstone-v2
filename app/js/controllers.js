debc2b4ddeac0c00760bab239ff0f8577ae8d4e5
shanghaiellen <shanghaiellen@gmail.com>
2014-04-07 16:06:24 -0700

    some form styling, restangular does not actually make calls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,4 +2,8 @@ var epicookbookControllers = angular.module('epicookbookControllers', []);
 
 epicookbookControllers.controller('HomeController', ['$scope', 'Restangular',
   function($scope, Restangular){
+    var getRecipes = Restangular.all('http://localhost:3000/recipes');
+    getRecipes.getList().then(function(someRecipes){
+      $scope.recipes = someRecipes;
+    });
   }]);
